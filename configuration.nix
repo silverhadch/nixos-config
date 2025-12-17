afd035782f8aff634c0e372677b1b18ad8843901
Hadi Chokr <hadichokr@icloud.com>
2025-12-17 19:14:18 +0100

    Wrap Webex to work properly.

--- configuration.nix
+++ configuration.nix
@@ -20,8 +20,22 @@ in
   nixpkgs.overlays = [
     (self: super: {
       bottles = super.bottles.override { removeWarningPopup = true; };
+
+      webex = super.webex.overrideAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ super.makeWrapper ];
+
+        postFixup = (old.postFixup or "") + ''
+          wrapProgram $out/opt/Webex/bin/CiscoCollabHost \
+            --set WAYLAND_DISPLAY "" \
+            --set XDG_SESSION_TYPE x11 \
+            --set QT_QPA_PLATFORM xcb \
+            --set GDK_BACKEND x11 \
+            --set NIXOS_OZONE_WL 0
+        '';
+      });
     })
   ];
+
 
   programs.nix-ld.enable = true;
 
