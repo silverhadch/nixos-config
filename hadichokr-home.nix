2a0f73cb4b99f46a945fb586e486c71d17744b83
Hadi Chokr <hadichokr@icloud.com>
2025-12-17 17:30:11 +0100

    Flatpak override.

--- hadichokr-home.nix
+++ hadichokr-home.nix
@@ -211,6 +211,31 @@ in
       "org.zealdocs.Zeal"
       "party.supertux.supertuxparty"
     ];
+    overrides = {
+      global = {
+        # Force Wayland by default
+        Context.sockets = ["wayland" "!x11" "!fallback-x11"];
+
+        Environment = {
+          # Fix un-themed cursor in some Wayland apps
+          XCURSOR_PATH = "/run/host/user-share/icons:/run/host/share/icons";
+
+          # Force correct theme for some GTK apps
+          GTK_THEME = "Adwaita:dark";
+        };
+      };
+
+      "com.ktechpit.whatsie".Context = {
+        filesystems = [
+          "home:rw" # Expose user
+          "/run/current-system/sw/bin:ro" # Expose NixOS managed software
+        ];
+        sockets = [
+          "gpg-agent" # Expose GPG agent
+          "pcsc" # Expose smart cards (i.e. YubiKey)
+        ];
+      };
+    };
     update.auto = {
       enable     = true;
       onCalendar = "daily";
@@ -250,16 +275,4 @@ in
       rows   = 2;
     };
   };
-
-  ## ── Custom activation hooks ────────────────────────────────────────
-  home.activation.set-plasma-startmenu-icon = ''
-    echo "Setting Plasma Application Launcher icon…"
-    section='[Containments][2][Applets][3][Configuration][General]'
-    config="$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc"
-    cp "$config" "$config.home-manager-backup"
-    grep -q "^$section" "$config" || echo "$section" >> "$config"
-    grep -q "^icon=" "$config" && \
-      sed -i "/^$section/,/^\[/ s/^icon=.*/icon=nix-snowflake/" "$config" || \
-      sed -i "/^$section/a icon=nix-snowflake" "$config"
-  '';
 }
