57dd14699976833dc8454b8a7b19782e1c3f27f3
Hadi Chokr <hadichokr@icloud.com>
2026-02-09 15:21:34 +0100

    Update inputs on 2026-02-09 15:21

--- shared/configuration.nix
+++ shared/configuration.nix
@@ -365,19 +365,19 @@
           removeWarningPopup = true;
         };
 
-        webex = super.webex.overrideAttrs (old: {
-          nativeBuildInputs =
-            (old.nativeBuildInputs or []) ++ [ super.makeWrapper ];
-
-          postFixup = (old.postFixup or "") + ''
-            wrapProgram $out/opt/Webex/bin/CiscoCollabHost \
-              --set WAYLAND_DISPLAY "" \
-              --set XDG_SESSION_TYPE x11 \
-              --set QT_QPA_PLATFORM xcb \
-              --set GDK_BACKEND x11 \
-              --set NIXOS_OZONE_WL 0
-          '';
-        });
+#         webex = super.webex.overrideAttrs (old: {
+#           nativeBuildInputs =
+#             (old.nativeBuildInputs or []) ++ [ super.makeWrapper ];
+#
+#           postFixup = (old.postFixup or "") + ''
+#             wrapProgram $out/opt/Webex/bin/CiscoCollabHost \
+#               --set WAYLAND_DISPLAY "" \
+#               --set XDG_SESSION_TYPE x11 \
+#               --set QT_QPA_PLATFORM xcb \
+#               --set GDK_BACKEND x11 \
+#               --set NIXOS_OZONE_WL 0
+#           '';
+#         });
       })
     ];
   };
