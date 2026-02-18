7e718ceb37a0cc1b82d86d9bf005fc1b00acacbb
Hadi Chokr <hadichokr@icloud.com>
2026-02-18 14:32:23 +0100

    Update inputs on 2026-02-18 14:32

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Hadi's NixOS desktop (flake)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
     home-manager = {
       url = "github:nix-community/home-manager";
