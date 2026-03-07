40a4d957b37930685cf3ec17540d4bab2a45df10
Hadi Chokr <hadichokr@icloud.com>
2026-03-07 20:43:33 +0100

    Update inputs on 2026-03-07 20:43

--- shared/modules/systemd.nix
+++ shared/modules/systemd.nix
@@ -9,47 +9,5 @@
     settings.Manager = {
       DefaultTimeoutStopSec = "10s";
     };
-
-    #
-    # User services
-    #
-    user = {
-
-      #
-      # Import graphical session environment into systemd --user
-      # This fixes xdg-desktop-portal / xdg-open on Budgie/Wayland
-      #
-      services.import-graphical-environment = {
-        description = "Import graphical session environment into systemd user";
-        wantedBy    = [ "graphical-session.target" ];
-        after       = [ "graphical-session.target" ];
-        serviceConfig = {
-          Type = "oneshot";
-          ExecStart = ''
-            ${pkgs.systemd}/bin/systemctl --user import-environment \
-              DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP \
-              XDG_SESSION_TYPE XDG_DATA_DIRS PATH
-          '';
-        };
-      };
-
-      #
-      # Polkit authentication agent
-      #
-      services.polkit-gnome-authentication-agent-1 = {
-        description = "polkit-gnome-authentication-agent-1";
-        wantedBy    = [ "graphical-session.target" ];
-        wants       = [ "graphical-session.target" ];
-        after       = [ "graphical-session.target" ];
-        serviceConfig = {
-          Type           = "simple";
-          ExecStart      = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-          Restart        = "on-failure";
-          RestartSec     = 1;
-          TimeoutStopSec = 10;
-        };
-      };
-
-    };
   };
 }
