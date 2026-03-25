be8ce8a648a908a5c103e536ccd7b3dcc629c870
Hadi Chokr <hadichokr@icloud.com>
2026-03-25 13:50:15 +0100

    Update inputs on 2026-03-25 13:50

--- shared/modules/no-spyware-here.nix
+++ shared/modules/no-spyware-here.nix
@@ -12,4 +12,26 @@ in {
   services.userdbd.enable = lib.mkForce false;
   systemd.services.systemd-userdbd.enable = lib.mkForce false;
   systemd.sockets.systemd-userdbd.enable = lib.mkForce false;
+
+  # disable parental portal
+  xdg.portal.config.common."org.freedesktop.impl.portal.ParentalControls" = "none";
+
+  # block AccountsService birthdate API
+  services.dbus.packages = [
+    (pkgs.writeTextFile {
+      name = "block-accountsservice-birthdate";
+      destination = "/share/dbus-1/system.d/block-accountsservice-birthdate.conf";
+      text = ''
+        <!DOCTYPE busconfig PUBLIC
+         "-//freedesktop//DTD D-BUS Bus Configuration 1.0//EN"
+         "http://www.freedesktop.org/standards/dbus/1.0/busconfig.dtd">
+        <busconfig>
+          <policy context="default">
+            <deny send_member="GetBirthDate"/>
+            <deny send_member="SetBirthDate"/>
+          </policy>
+        </busconfig>
+      '';
+    })
+  ];
 }
