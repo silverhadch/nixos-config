b2caeaf0b6e201ce1f36e375e969722860493c7d
Hadi Chokr <hadichokr@icloud.com>
2026-03-28 16:22:30 +0100

    Update inputs on 2026-03-28 16:22

--- shared/modules/binfmt.nix
+++ shared/modules/binfmt.nix
@@ -1,16 +1,22 @@
 { config, pkgs, ... }:
 
 {
-  boot.binfmt.registrations = {
-    dos = {
-      # Recognise DOS MZ executables by their magic bytes
-      recognitionType = "magic";
-      magicOrExtension = ''\x4d\x5a''; # MZ header
-      mask = ''\xff\xff'';             # Match both bytes
-      # Use DOSBox as the interpreter
-      interpreter = "${pkgs.dosbox}/bin/dosbox";
-      # Wrap in a shell to pass additional arguments like -exit
-      wrapInterpreterInShell = true;
+  boot.binfmt = {
+    preferStaticEmulators = true;
+    registrations = {
+      dos = {
+        # Recognise DOS MZ executables by their magic bytes
+        recognitionType = "magic";
+        magicOrExtension = ''\x4d\x5a''; # MZ header
+        mask = ''\xff\xff'';             # Match both bytes
+        # Use DOSBox as the interpreter
+        interpreter = "${pkgs.dosbox}/bin/dosbox";
+        # Wrap in a shell to pass additional arguments like -exit
+        wrapInterpreterInShell = true;
+      };
     };
   };
+
+  # Java
+  programs.java.binfmt.enable = true;
 }
