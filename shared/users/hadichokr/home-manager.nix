4468ec907c1dce0ac88add519fa3e4b5bf7387f2
Hadi Chokr <hadichokr@icloud.com>
2026-04-11 13:35:53 +0200

    Update inputs on 2026-04-11 13:35

--- shared/users/hadichokr/home-manager.nix
+++ shared/users/hadichokr/home-manager.nix
@@ -324,6 +324,26 @@
       export VISUAL=nvim
       export PATH=$HOME/.local/bin:$PATH
 
+      alias what-changes="echo 'What changes a lot?' && git log --format=format: --name-only --since='1 year ago' | rg -v 'po$|json$|desktop$' | sort | uniq -c | sort -nr | head -20"
+      alias what-breaks="echo 'What breaks a lot?' && git log -i -E --grep='fix|bug|broke|bad|wrong|incorrect|problem' --name-only --format="" | sort | uniq -c | sort -nr | head -20"
+      alias emergencies="echo 'And what were the emergencies?' && git log --oneline --since='1 year ago' | grep -iE 'revert|hotfix|emergency|urgent|rollback'"
+      alias momentum="echo \"What's the project's momentum over the past 5 years?\" && git log --format='%ad' --date=format:'%Y-%m' | sort | uniq -c | tail -n 60"
+      alias maintainers-recently="echo \"Who's been driving this project in the past year?\" && git shortlog -sn --no-merges --since='1 year ago' | rg -v 'l10n daemon script' | head -n 30"
+      alias maintainers-alltime="echo 'And what about for all time?' && git shortlog -sn --no-merges | rg -v 'l10n daemon script' | head -n 30"
+      function repo-analysis {
+          what-changes
+          echo
+          what-breaks
+          echo
+          emergencies
+          echo
+          momentum
+          echo
+          maintainers-recently
+          echo
+          maintainers-alltime
+      }
+
       clear
       fastfetch
     '';
