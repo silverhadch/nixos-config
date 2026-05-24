0f1d05fa346272d52fbf298639737f8bf95363e9
Hadi Chokr <hadichokr@icloud.com>
2026-05-24 12:53:24 +0200

    Update inputs on 2026-05-24 12:53

--- shared/modules/systemd.nix
+++ shared/modules/systemd.nix
@@ -2,10 +2,6 @@
 
 {
   systemd = {
-   additionalUpstreamSystemUnits = [
-    "soft-reboot.target"
-    "systemd-soft-reboot.service"
-  ];
     #
     # Global systemd manager settings (NixOS 26+ compatible)
     #
